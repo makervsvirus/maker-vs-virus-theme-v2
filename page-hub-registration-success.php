f2fa5f01210a37cc7f8cf0f96ef921c37eb62397
Jonathan Günz <jonathan.guenz@experimenta.science>
2020-04-02 02:46:33 +0200

    fix some design issues

--- page-hub-registration-success.php
+++ page-hub-registration-success.php
@@ -1,13 +1,12 @@
 <?php get_header(); ?>
 
-<?php require 'includes/navigation.php' ?>
 
 
 <?php
 
 $google_api_key = get_option('google_geocoding_api_key');
 $address = $_POST['hub_street'] . " " . $_POST["hub_zip"] . " " . $_POST["hub_city"] . " " . $_POST["hub_state"] . " " . $_POST["hub_country"];
-$url = "https://maps.googleapis.com/maps/api/geocode/json?sensor=false&key=" . $google_api_key . "&address=" . urlencode ( $address );
+$url = "https://maps.googleapis.com/maps/api/geocode/json?sensor=false&key=" . $google_api_key . "&address=" . urlencode($address);
 
 $header = array("Accept: application/json");
 
@@ -22,26 +21,26 @@ curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows 
 
 $response = curl_exec($ch);
 
-$result = json_decode( $response );
+$result = json_decode($response);
 
 $lat = $result->results[0]->geometry->location->lat;
 $long = $result->results[0]->geometry->location->lng;
 
 
-$random_password = wp_generate_password( $length = 12, $include_standard_special_chars = true );
+$random_password = wp_generate_password($length = 12, $include_standard_special_chars = true);
 $user_id = wp_create_user($_POST['hub_email'], $random_password, $_POST['hub_email']);
 
-$u = new WP_User( $user_id );
-$u->remove_role( 'subscriber' );
-$u->add_role( 'author' );
+$u = new WP_User($user_id);
+$u->remove_role('subscriber');
+$u->add_role('author');
 
 
-$post_id = wp_insert_post( array(
+$post_id = wp_insert_post(array(
     'post_author'           => $user_id,
     'post_title'            => $_POST['hub_name'],
     'post_type'             => 'mvv_hub',
     'post_name'             => $_POST['hub_city'] . '_' . $_POST['hub_name']
-) );
+));
 
 
 update_post_meta($post_id, "hub_lat", $lat);
@@ -74,25 +73,28 @@ foreach ($fields as $field) {
 
 ?>
 
-<section>
-    <div class="container flex">
-        <div class="text editable">
-            <h2><?php pll_e('hub_registration_success_message_header') ?></h2>
-            <p><?php pll_e('hub_registration_success_message') ?></p>
-        </div>
-    </div>
-    <div class="container flex" style="margin-top: 1rem;">
-    <div class="text editable">
-            <div class="button">
-                <a style="background: {{ plan.color }}" href="https://join.slack.com/t/makervsvirus/shared_invite/zt-d0jrseye-16B4eZm1lkX~Hi8V7zFbiw" target="_blank">
-                    <?php pll_e('support_join_slack') ?></a>
+<div style="min-height: 100vh;">
+    <div class="bg-white" style="padding-bottom: 2rem;">
+
+
+        <div class="clr-row">
+            <div class="clr-col-lg-8 clr-offset-lg-2">
+                <?php while (have_posts()) : the_post(); ?>
+                    <h1><?php the_title() ?></h1>
+                    <p>
+                        <?php the_content(); ?>
+                    </p>
+                <?php endwhile; ?>
+            </div>
+
+            <div class="clr-col-lg-8 clr-offset-lg-2 style=" margin-top: 1rem;">
+
+                <a href="https://join.slack.com/t/makervsvirus/shared_invite/zt-d0jrseye-16B4eZm1lkX~Hi8V7zFbiw" target="_blank">
+                    <button class="btn btn-primary">Join Slack</button>
+                </a>
             </div>
         </div>
-        <div class="text editable">
-            <div class="button">
-                <a style="background: {{ plan.color }}" href="/wp-admin" target="_blank">
-                    <?php pll_e('login_to_wordpress') ?></a>
-            </div>
-        </div>
     </div>
-</section>
+</div>
+
+<?php get_footer(); ?>
--- page-hubs.php
+++ page-hubs.php
@@ -1,70 +1,72 @@
 <?php get_header(); ?>
 
-<?php require 'includes/navigation.php' ?>
+<div class="bg-white">
 
 
-<?php require 'includes/map.php' ?>
+
+    <?php require 'includes/map.php' ?>
 
 
-<?php
+    <?php
 
-$posts = get_posts(array(
-    'post_type'         => 'mvv_hub',
-    'posts_per_page'    =>  -1,
-    'orderby'           => 'title',
-    'order'              => 'ASC'
-));
+    $posts = get_posts(array(
+        'post_type'         => 'mvv_hub',
+        'posts_per_page'    =>  -1,
+        'orderby'           => 'title',
+        'order'              => 'ASC'
+    ));
 
-?>
+    ?>
 
 
-<div class="content-container">
-    <div class="content-area">
-        <div class="clr-row">
-            <div class="clr-col-lg-8 clr-offset-lg-2">
+    <div class="content-container">
+        <div class="content-area">
+            <div class="clr-row">
+                <div class="clr-col-lg-8 clr-offset-lg-2">
 
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Ort</th>
-                            <th>Wir bieten</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($posts as $post) : ?>
-
+                    <table class="table">
+                        <thead>
                             <tr>
-                                <td>
-                                    <a href="<?php echo get_permalink() ?>"><?php echo get_the_title() ?></a>
-                                    <br />&nbsp;<br />
-
-                                    <?php echo get_post_meta($post->ID, 'hub_contact_person', true) ?><br />
-                                    <?php echo get_post_meta($post->ID, 'hub_email', true) ?><br />
-                                    <?php echo get_post_meta($post->ID, 'hub_twitter', true) ?><br />
-                                </td>
-                                <td>
-                                    <?php echo get_post_meta($post->ID, 'hub_city', true) ?><br />
-                                    <?php echo get_post_meta($post->ID, 'hub_state', true) ?><br />
-                                    <?php echo get_post_meta($post->ID, 'hub_country', true) ?>
-                                </td>
-                                <td>
-                                    <b>Wir bieten</b><br />
-                                    <?php echo get_post_meta($post->ID, 'hub_offer', true) ?>
-
-                                    <br />&nbsp;<br />
-                                    <b>Kapazitäten</b><br />
-                                    <?php echo get_post_meta($post->ID, 'hub_capacity', true) ?>
-                                </td>
+                                <th>Name</th>
+                                <th>Ort</th>
+                                <th>Wir bieten</th>
                             </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($posts as $post) : ?>
 
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
+                                <tr>
+                                    <td>
+                                        <a href="<?php echo get_permalink() ?>"><?php echo get_the_title() ?></a>
+                                        <br />&nbsp;<br />
 
+                                        <?php echo get_post_meta($post->ID, 'hub_contact_person', true) ?><br />
+                                        <?php echo get_post_meta($post->ID, 'hub_email', true) ?><br />
+                                        <?php echo get_post_meta($post->ID, 'hub_twitter', true) ?><br />
+                                    </td>
+                                    <td>
+                                        <?php echo get_post_meta($post->ID, 'hub_city', true) ?><br />
+                                        <?php echo get_post_meta($post->ID, 'hub_state', true) ?><br />
+                                        <?php echo get_post_meta($post->ID, 'hub_country', true) ?>
+                                    </td>
+                                    <td>
+                                        <b>Wir bieten</b><br />
+                                        <?php echo get_post_meta($post->ID, 'hub_offer', true) ?>
+
+                                        <br />&nbsp;<br />
+                                        <b>Kapazitäten</b><br />
+                                        <?php echo get_post_meta($post->ID, 'hub_capacity', true) ?>
+                                    </td>
+                                </tr>
+
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+
+                </div>
             </div>
-        </div>
 
+        </div>
     </div>
 </div>
 
