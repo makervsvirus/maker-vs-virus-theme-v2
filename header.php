172f1587f76bfc9cffaded939b8509f501be9c2a
Benedikt Hübschen <benedikthubschen@MacBook-Pro.edling.bayern>
2020-04-06 06:30:04 +0200

    favicons + hub-mail

--- header.php
+++ header.php
@@ -19,16 +19,14 @@
 	<!--CLARITY ICONS API & ALL ICON SETS-->
 	<script src="<?php echo get_template_directory_uri() ?>/assets/js/clr-icons.min.js"></script>
 
-	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() ?>/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri() ?>/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri() ?>/favicon-16x16.png">
-	<link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri() ?>/favicon.ico" />
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() ?>/assets/images/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri() ?>/assets/images/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri() ?>/assets/images/favicon-16x16.png">
+	<link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri() ?>/assets/images/favicon.ico" />
 	<link rel="manifest" href="site.webmanifest">
-	<link rel="mask-icon" href="safari-pinned-tab.svg" color="#4182e4">
-	<meta name="msapplication-TileColor" content="#2d89ef">
+	<link rel="mask-icon" href="<?php echo get_template_directory_uri() ?>/assets/images/safari-pinned-tab.svg" color="#4182e4">
+	<meta name="msapplication-TileColor" content="#4182e4">
 	<meta name="theme-color" content="#ffffff">
-
-	<meta name="test" content="01:45">
 
 </head>
 
