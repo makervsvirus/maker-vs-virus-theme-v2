b91de7d24afdc92d45e08055a7246cbd5be7a0f1
Benedikt Hübschen <benedikthubschen@Benedikts-MacBook-Pro-2.local>
2020-04-07 16:13:58 +0200

    rudimental 404

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+<section class="hero">
+    <h1 style="color:white; font-size:250%">404 - Seite nicht gefunden</h1>
+</section>
+<div class="bg-white">
+    <div class="">
+        <div id="main" style="text-align:center">
+            <br>
+            <img src="<?php echo get_template_directory_uri() ?>/assets/images/filament-spool.png" style="height: 60vh">
+            <p>Tut uns leid, aber die Seite nach der du gesucht hast, wurde leider nicht gefunden.</p>
+        </div><!-- main -->
+    </div><!-- main -->
+</div><!-- main -->
+
+<?php get_footer(); ?>
